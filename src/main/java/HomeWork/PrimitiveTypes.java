23a81057479d2acb453dbdc4710dfed26c07fac2
Alex <alex.v77@inbox.lv>
2022-01-02 11:38:15 +0200

    [feature/java2]:Classroom progress add.

---
+++ src/main/java/HomeWork/PrimitiveTypes.java
@@ -0,0 +1,410 @@
+package HomeWork;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+
+//        Example of variables
+
+        //float
+
+        float populationOnSaulkrastiIn1997 = 5.528F;
+        System.out.println(populationOnSaulkrastiIn1997);
+
+        float populationOnSaulkrastiIn1998 = 5.560F;
+        System.out.println(populationOnSaulkrastiIn1998);
+
+        float populationOnSaulkrastiIn1999 = 5.541F;
+        System.out.println(populationOnSaulkrastiIn1999);
+
+        float populationOnSaulkrastiIn2000 = 5.532F;
+        System.out.println(populationOnSaulkrastiIn2000);
+
+        float populationOnSaulkrastiIn2001 = 5.427F;
+        System.out.println(populationOnSaulkrastiIn2001);
+
+        float populationOnSaulkrastiIn2002 = 5.463F;
+        System.out.println(populationOnSaulkrastiIn2002);
+
+        float populationOnSaulkrastiIn2003 = 5.403F;
+        System.out.println(populationOnSaulkrastiIn2003);
+
+        float populationOnSaulkrastiIn2004 = 5.651F;
+        System.out.println(populationOnSaulkrastiIn2004);
+
+        float populationOnSaulkrastiIn2005 = 5.926F;
+        System.out.println(populationOnSaulkrastiIn2005);
+
+        float populationOnSaulkrastiIn2006 = 6.032F;
+        System.out.println(populationOnSaulkrastiIn2006);
+
+        //float limit
+
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+        //double
+
+        //unit conversion
+
+        double meter2PerAcre = 4046.8564224D;
+        System.out.println(meter2PerAcre);
+
+        double degreesCelsiusToOneDegreeFahrenheit = -17.2222222222D;
+        System.out.println(degreesCelsiusToOneDegreeFahrenheit);
+
+        double degreesKelvinToOneDegreeFahrenheit = 255.9277777778D;
+        System.out.println(degreesKelvinToOneDegreeFahrenheit);
+
+        double litersToUSGallon = 3.785411784D;
+        System.out.println(litersToUSGallon);
+
+        double americanGallonsToImperialGallons = 1.2009499255D;
+        System.out.println(americanGallonsToImperialGallons);
+
+        double megabytesPerKilobyte = 0.0009765625D;
+        System.out.println(megabytesPerKilobyte);
+
+        double yardsPerFoot = 0.3333333333D;
+        System.out.println(yardsPerFoot);
+
+        double nauticalMilesPerMile = 0.8689762419D;
+        System.out.println(nauticalMilesPerMile);
+
+        double hoursPerSecond = 0.0002777778D;
+        System.out.println(hoursPerSecond);
+
+        double kilogramsPerPound = 0.45359237D;
+        System.out.println(kilogramsPerPound);
+
+        //double limit
+
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+//       integral example
+
+        //byte example
+
+        byte eyesCount = 2;
+        System.out.println(eyesCount);
+
+        byte wheelsCountsInTheCar = 4;
+        System.out.println(wheelsCountsInTheCar);
+
+        byte numberOfBitsInBytes = 8;
+        System.out.println(numberOfBitsInBytes);
+
+        byte numberOfCentimetersInOneMeter = 100;
+        System.out.println(numberOfCentimetersInOneMeter);
+
+        byte numberOfEntrancesInTheHouse = 5;
+        System.out.println(numberOfEntrancesInTheHouse);
+
+        byte stringsCountsForAcousticGuitar = 6;
+        System.out.println(stringsCountsForAcousticGuitar);
+
+        byte studentsCountsInTheClass = 20;
+        System.out.println(studentsCountsInTheClass);
+
+        byte numberOfSoldiersInCompany = 127;
+        System.out.println(numberOfSoldiersInCompany);
+
+        byte freezingPointOfWater = 0;
+        System.out.println(freezingPointOfWater);
+
+        byte numberOfmMetersFromHomeToShop = 126;
+        System.out.println(numberOfmMetersFromHomeToShop);
+
+        //byte limit
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+         //short example
+
+        short freezingPointOfLiquidNitrogenInCelsius = -210;
+        System.out.println(freezingPointOfLiquidNitrogenInCelsius);
+
+        short boilingPointOfLiquidNitrogenInCelsius = -196;
+        System.out.println(boilingPointOfLiquidNitrogenInCelsius);
+
+        short diameterOfPlanetEarth = 12742;
+        System.out.println(diameterOfPlanetEarth);
+
+        short diameterOfPlanetVenus = 12103;
+        System.out.println(diameterOfPlanetVenus);
+
+        short circumferenceOfMars = 21344;
+        System.out.println(circumferenceOfMars);
+
+        short populationOnRezekne = 32328;
+        System.out.println(populationOnRezekne);
+
+        short distanceBetweenRigaAndAdaziInMeters = 19800;
+        System.out.println(distanceBetweenRigaAndAdaziInMeters);
+
+        short populationRepublicOfPalau = 21507;
+        System.out.println(populationRepublicOfPalau);
+
+        short lengthTheAmazonRive = 6992;
+        System.out.println(lengthTheAmazonRive);
+
+        short lengthTheNile = 6693;
+        System.out.println(lengthTheNile);
+
+        //short limit
+
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+
+        //integer example
+
+        int populationOnChina = 1411778724;
+        System.out.println(populationOnChina);
+
+        int distanceBetweenSunAndSaturn = 1429400028;
+        System.out.println(distanceBetweenSunAndSaturn);
+
+        int squareOfLatviaInMeters = 64589000;
+        System.out.println(squareOfLatviaInMeters);
+
+        int distanceBetweenEarthToMoon = 384400;
+        System.out.println(distanceBetweenEarthToMoon);
+
+        int populationOnRussia = 146171015;
+        System.out.println(populationOnRussia);
+
+        int numberOfToyotaCarsSalesIn2020 = 7586281;
+        System.out.println(numberOfToyotaCarsSalesIn2020);
+
+        int equatorLength = 40075;
+        System.out.println(equatorLength);
+
+        int populationOnUsa = 335026667;
+        System.out.println(populationOnUsa);
+
+        int distanceBetweenSunAndVenus = 108208000;
+        System.out.println(distanceBetweenSunAndVenus);
+
+        int distanceBetweenSunAndMercury = 57910006;
+        System.out.println(distanceBetweenSunAndMercury);
+
+        //integer limit
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+
+        //long example
+
+        long populationOfTheEarth = 7948063330L;
+        System.out.println(populationOfTheEarth);
+
+        long malePopulationOfTheEarth = 3961397005L;
+        System.out.println(malePopulationOfTheEarth);
+
+        long womanPopulationOfTheWorld = 3889869083L;
+        System.out.println(womanPopulationOfTheWorld);
+
+        long theNumberOfPeopleInfectedWithCoronavirus = 288538474L;
+        System.out.println(theNumberOfPeopleInfectedWithCoronavirus);
+
+        long theNumberOfPeopleWhoRecoveredFromTheCoronavirus = 253714393L;
+        System.out.println(theNumberOfPeopleWhoRecoveredFromTheCoronavirus);
+
+        long deathsFromCoronavirus = 5453217L;
+        System.out.println(deathsFromCoronavirus);
+
+        long areSickNowWithCoronavirus = 29370864L;
+        System.out.println(areSickNowWithCoronavirus);
+
+        long areSickNowWithCoronavirusInRussia = 727203L;
+        System.out.println(areSickNowWithCoronavirusInRussia);
+
+        long theNumberOfPeopleInfectedWithCoronavirusInRussia = 10499982L;
+        System.out.println(theNumberOfPeopleInfectedWithCoronavirusInRussia);
+
+        long deathsFromCoronavirusInRussia = 308860L;
+        System.out.println(deathsFromCoronavirusInRussia);
+
+        //long limit
+
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+       // char example
+
+        char a1 = 243;
+        System.out.println(a1);
+
+        char zah2 = 97;
+        System.out.println(zah2);
+
+        char numberOfCode = '8';
+        System.out.println(numberOfCode);
+
+        char gateName = 'C';
+        System.out.println(gateName);
+
+        char exclamationPoint = 33;
+        System.out.println(exclamationPoint);
+
+        char houseNumber = '2';
+        System.out.println(houseNumber);
+
+        char c1 = 74;
+        char c2 = 65;
+        char c3 = 86;
+        System.out.println(c1+""+c2+""+c3+""+c2);
+
+        char smile1 = '☺'; //alt+1
+        char smile2 = '☻'; //alt+2
+        System.out.println(smile1 + " " + smile2);
+
+        char unicodeSymbol = 't';
+        System.out.println(unicodeSymbol);
+        System.out.println((int) unicodeSymbol);
+
+        char unicodeSymbol1 = 116;
+        System.out.println(unicodeSymbol1);
+
+        //char limit
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+
+//        arithmetic operators
+
+        //addition
+
+        int a = 2;
+        int b = 3;
+        int c = a+b;
+        System.out.println(c);
+
+       int sumOfTwoNumber = 20 + 100;
+        System.out.println(sumOfTwoNumber);
+
+        //subtraction
+
+        int x =150;
+        int y = 120;
+        int z = x-y;
+        System.out.println(z);
+
+        int r = 100 - 50;
+        System.out.println(r);
+
+        //multiplication
+
+        int i = 22;
+        int m = 2;
+        int k = i * m;
+        System.out.println(k);
+
+        int w = 320 * 10;
+        System.out.println(w);
+
+        //division
+
+        int g = 150;
+        int l = 3;
+        int u = g / l;
+        System.out.println(u);
+
+        int q = 200 / 100;
+        System.out.println(q);
+
+        //remainder
+
+        int o = 10;
+        int v = 3;
+        int p = o % v;
+        System.out.println(p);
+
+        int j = 150 % 60;
+        System.out.println(j);
+
+//        string example
+
+        String myName = "Alexander";
+        System.out.println(myName);
+
+        String mySerName = "Vorobjov";
+        System.out.println(mySerName);
+
+        String myDaughtersName = "Maja";
+        System.out.println(myDaughtersName);
+
+        String myWiveName = "Viktorija";
+        System.out.println(myWiveName);
+
+        String myCountry = "Latvia";
+        System.out.println(myCountry);
+
+        String myNameAndUsername = myName + " " + mySerName;
+        System.out.println(myNameAndUsername);
+
+//        example sentence
+
+//        Hi, my name is Alexander. I dream of working in IT.
+
+        String firstPart = "Hi, my  name is Alexandr.";
+        System.out.println(firstPart);
+
+        String secondPart = "I dream of working in IT.";
+        System.out.println(secondPart);
+
+        System.out.printf("%s%s\n", firstPart, secondPart);
+        System.out.println("Hi, my name is Alexander" + "." + "I dream of working in IT");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
