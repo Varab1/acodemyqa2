5dc9abccfce3520b360ffcacecfffa9fd184e61d
Alex <alex.v77@inbox.lv>
2022-01-14 20:42:50 +0200

    [feature/java2]:Add MultiplicationTable file

---
+++ src/test/java/classroom/MultiplicationTable.java
@@ -0,0 +1,51 @@
+package classroom;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int c = 2; c < 22; c = c + 2) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 3; c < 33; c = c + 3) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 4; c < 44; c = c + 4) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 5; c < 55; c = c + 5) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 6; c < 66; c = c + 6) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 7; c < 77; c = c + 7) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 8; c < 88; c = c + 8) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 9; c < 99; c = c + 9) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
+
+        for (int c = 10; c < 110; c = c + 10) {
+            System.out.print(c + " ");
+        }
+
+    }
+}
