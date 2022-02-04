54e832fb7685be6e7411b73d071004c630792edc
Alex <alex.v77@inbox.lv>
2022-02-04 18:11:30 +0200

    [feature/java4]:Classroom progress add

---
+++ src/main/java/classroom/Calculator.java
@@ -0,0 +1,35 @@
+package classroom;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class Calculator {
+
+    @Getter
+    private String modelName;
+
+
+    public int sum(int x, int y) {
+        return x + y;
+    }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+
+
+
+    public int squared(int x) {
+        return x * x;
+    }
+}
