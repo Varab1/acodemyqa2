e23bf618f9f88175793c43510b40c4330a477d98
Alex <alex.v77@inbox.lv>
2022-03-06 13:01:30 +0200

    [master]:Jenkins

--- src/test/java/QuantityTest.java
+++ src/test/java/QuantityTest.java
@@ -3,12 +3,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import page_object.*;
+import page_object.CartPage;
+import page_object.HeaderPage;
+import page_object.HomePage;
+import page_object.ProductPage;
 import utils.LocalDriverManager;
 import utils.PropertiesReader;
 
-import static constans.Colors.BLUE;
-import static constans.Products.HOODIE;
+import static constans.Products.BELT;
 
 
 @Slf4j
@@ -20,7 +22,7 @@ public class QuantityTest {
     ProductPage productPage = new ProductPage();
     CartPage cartPage = new CartPage();
 
-    @BeforeEach       // Инициализация теста.
+    @BeforeEach
     public void init() {
         log.info("Step 1: User open the Online shop page.");
         driver.get(PropertiesReader.getProperties().getProperty("home.page"));
@@ -29,24 +31,18 @@ public class QuantityTest {
     @Test
     public void applyCouponCode() {
         log.info("Step 2: User select any product on the main page");
-        homePage.clickOnProduct(HOODIE);
+        homePage.clickOnProduct(BELT);
 
-        log.info("Step 3: User select color");
-        productPage.selectColor(BLUE);
-
-        log.info("Step 4: User select logo");
-        productPage.selectLogo(true);
-
-        log.info("Step 5: User adding product to cart");
+        log.info("Step 3: User adding product to cart");
         productPage.addProductToCart();
 
-        log.info("Step 6: User click cart Button");
+        log.info("Step 4: User click cart Button");
         productPage.viewCartButton();
 
-        log.info("Step 7: User changes the quantity of a product in the cart");
+        log.info("Step 5: User changes the quantity of a product in the cart");
         cartPage.checkQuantity(5);
 
-        log.info("Step 8: User clicks button UpdateCart");
+        log.info("Step 6: User clicks button UpdateCart");
         cartPage.updateCart();
     }
 
